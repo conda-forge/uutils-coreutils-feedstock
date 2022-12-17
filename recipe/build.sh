60fd78f96f4703bcce415ba8c5c25e4bbad753b8
Kerry Su <me@sshockwave.net>
2022-12-17 11:34:30 +0800

    Add --target compile option

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ if [[ "${target_platform}" == "osx"* ]]; then
     FEATURE_SET="macos"
 fi
 
-cargo build --release --features "${FEATURE_SET}"
+cargo build --release --target="${CARGO_BUILD_TARGET}" --features "${FEATURE_SET}"
 
 make PROFILE=Release PREFIX="${PREFIX}" PROG_SUFFIX= MULTICALL=y install
 
