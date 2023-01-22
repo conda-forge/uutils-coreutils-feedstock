778eab019e934084eefc8a121de08b4bfc3bbe3d
Kerry Su <me@sshockwave.net>
2023-01-22 14:38:53 +0800

    Update pre-unlink.sh

--- recipe/pre-unlink.sh
+++ recipe/pre-unlink.sh
@@ -4,8 +4,10 @@ set -ex
 echo Unlinking coreutils # debug
 util_bin="$CONDA_PREFIX"/bin/coreutils
 util_list="$("$util_bin" --help | tail -n +7)"
+util_list="${util_list//[[:blank:]]/}"
+util_list="${util_list//,/ }"
 
-for i in ${util_list//,/ }
+for i in $util_list
 do
   rm "$CONDA_PREFIX"/share/zsh/site-functions/_"$i"
   rm "$CONDA_PREFIX"/share/bash-completion/completions/"$i"
