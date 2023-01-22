6bf5710673dc11e729b5673e50248c9e96d563a9
Kerry Su <me@sshockwave.net>
2023-01-22 13:46:20 +0800

    Create pre-unlink.sh

---
+++ recipe/pre-unlink.sh
@@ -0,0 +1,13 @@
+set -ex
+
+# Install Shell completion
+echo Unlinking coreutils # debug
+util_bin="$CONDA_PREFIX"/bin/coreutils
+util_list="$("$util_bin" --help | tail -n +7)"
+
+for i in ${util_list//,/ }
+do
+  rm "$CONDA_PREFIX"/share/zsh/site-functions/_"$i"
+  rm "$CONDA_PREFIX"/share/bash-completion/completions/"$i"
+  rm "$CONDA_PREFIX"/share/fish/vendor_completions.d/"$i".fish
+done
