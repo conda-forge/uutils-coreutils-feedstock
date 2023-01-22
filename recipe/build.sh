55732e6921c0bb4c2027b3bf3aaf658eade43964
Kerry Su <me@sshockwave.net>
2023-01-22 13:28:14 +0800

    Add back completion on supported platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,4 +9,9 @@ cargo build --release --features "${FEATURE_SET}"
 
 make PROFILE=Release PREFIX="${PREFIX}" PROG_SUFFIX= MULTICALL=y install
 
+if [[ "${build_platform}" == "${target_platform}" ]]
+then
+    make PROFILE=Release PREFIX="${PREFIX}" PROG_SUFFIX= MULTICALL=y completion
+fi
+
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
