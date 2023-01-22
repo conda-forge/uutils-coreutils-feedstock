cc13d1364a830ef7c9e8e6faf1e26e47ace73e4b
Kerry Su <me@sshockwave.net>
2023-01-22 15:00:35 +0800

    Remove debug info

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,7 +1,5 @@
 set -ex
 
-# Install Shell completion
-echo Linking coreutils # debug
 util_bin="$CONDA_PREFIX"/bin/coreutils
 util_list="$("$util_bin" --help | tail -n +7)"
 util_list="${util_list//[[:space:]]/}"
@@ -13,7 +11,6 @@ mkdir -p "$CONDA_PREFIX"/share/fish/vendor_completions.d/
 
 for i in $util_list
 do
-  echo $i # debug
   "$util_bin" completion "$i" zsh > "$CONDA_PREFIX"/share/zsh/site-functions/_"$i"
   "$util_bin" completion "$i" bash > "$CONDA_PREFIX"/share/bash-completion/completions/"$i"
   "$util_bin" completion "$i" fish > "$CONDA_PREFIX"/share/fish/vendor_completions.d/"$i".fish
