5a9a7d41c5a473089de0896d4475b7639feb6027
Brandon Maier <brandon.maier@gmail.com>
2025-02-13 08:43:53 -0600

    attempt to install utils on windows

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,5 +1,11 @@
 cargo install --bins --no-track --locked --root "%LIBRARY_PREFIX%" --path . --features windows
 if errorlevel 1 exit 1
 
+%LIBRARY_PREFIX%\bin\coreutils.exe --list >utils.txt
+if errorlevel 1 exit 1
+
+for /F "tokens=*" %%i in (utils.txt) do xcopy %LIBRARY_PREFIX%\bin\coreutils.exe %LIBRARY_PREFIX%\bin\%%i.exe
+if errorlevel 1 exit 1
+
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 if errorlevel 1 exit 1
