75e9f780b852b41b6032a92377af8dd5c217cbee
Brandon Maier <brandon.maier@gmail.com>
2025-02-12 19:15:57 -0600

    drop makefile build

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,7 +1,4 @@
-cargo build --release --features windows
-if errorlevel 1 exit 1
-
-make PROFILE=Release PREFIX="%CYGWIN_PREFIX%/Library" MULTICALL=y PROG_SUFFIX=.exe install
+cargo install --bins --no-track --locked --root "%LIBRARY_PREFIX%" --path . --features windows
 if errorlevel 1 exit 1
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,22 @@ if [[ "${target_platform}" == "osx"* ]]; then
     FEATURE_SET="macos"
 fi
 
-cargo build --release --features "${FEATURE_SET}"
-
-make PROFILE=Release PREFIX="${PREFIX}" PROG_SUFFIX= MULTICALL=y install
+cargo install --bins --no-track --locked --root "${PREFIX}" --path . --features "${FEATURE_SET}"
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+coreutils_bin="$PREFIX/bin/coreutils"
+
+mkdir -p "$PREFIX/share/zsh/site-functions/"
+mkdir -p "$PREFIX/share/bash-completion/completions/"
+mkdir -p "$PREFIX/share/fish/vendor_completions.d/"
+mkdir -p "$PREFIX/share/man/man1"
+
+"$coreutils_bin" --list >utils
+while IFS= read -r util; do
+    ln -sf coreutils "$PREFIX/bin/$util"
+    "$coreutils_bin" completion "$util" zsh > "$PREFIX/share/zsh/site-functions/_$util"
+    "$coreutils_bin" completion "$util" bash > "$PREFIX/share/bash-completion/completions/$util"
+    "$coreutils_bin" completion "$util" fish > "$PREFIX/share/fish/vendor_completions.d/$util.fish"
+    "$coreutils_bin" manpage "$util" > "$PREFIX/share/man/man1/$util.1"
+done <utils
