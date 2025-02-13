10729bd623c22bb3e11840b5f5f4a304ef697a96
Brandon Maier <brandon.maier@gmail.com>
2025-02-13 08:59:04 -0600

    fix copy command

--- recipe/build.bat
+++ recipe/build.bat
@@ -4,7 +4,7 @@ if errorlevel 1 exit 1
 %LIBRARY_PREFIX%\bin\coreutils.exe --list >utils.txt
 if errorlevel 1 exit 1
 
-for /F "tokens=*" %%i in (utils.txt) do xcopy %LIBRARY_PREFIX%\bin\coreutils.exe %LIBRARY_PREFIX%\bin\%%i.exe
+for /F "tokens=*" %%i in (utils.txt) do cp %LIBRARY_PREFIX%\bin\coreutils.exe %LIBRARY_PREFIX%\bin\%%i.exe
 if errorlevel 1 exit 1
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
