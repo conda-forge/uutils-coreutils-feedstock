4486b87c7e158cb6b0224c720cc026626597c93b
Brandon Maier <brandon.maier@gmail.com>
2025-02-17 13:23:03 -0600

    switch back to makefile build

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,10 +1,4 @@
-cargo install --bins --no-track --locked --root "%LIBRARY_PREFIX%" --path . --features windows
-if errorlevel 1 exit 1
-
-%LIBRARY_PREFIX%\bin\coreutils.exe --list >utils.txt
-if errorlevel 1 exit 1
-
-for /F "tokens=*" %%i in (utils.txt) do copy %LIBRARY_PREFIX%\bin\coreutils.exe %LIBRARY_PREFIX%\bin\%%i.exe
+make PROFILE=release PREFIX="%CYGWIN_PREFIX%/Library" MULTICALL=y PROG_SUFFIX=.exe install
 if errorlevel 1 exit 1
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,30 +1,15 @@
 set -ex
 
-FEATURE_SET="unix"
-if [[ "${target_platform}" == "osx"* ]]; then
-    FEATURE_SET="macos"
+MANPAGES=
+COMPLETIONS=
+
+# Generating manpages and completions requires calling the built tools
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+    MANPAGES=y
+    COMPLETIONS=y
 fi
 
-cargo install --bins --no-track --locked --root "${PREFIX}" --path . --features "${FEATURE_SET}"
+make PROFILE=release PREFIX="${PREFIX}" PROG_SUFFIX= MULTICALL=y \
+    MANPAGES=${MANPAGES} COMPLETIONS=${COMPLETIONS} install
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-
-while read -r util; do
-    ln -sf coreutils "$PREFIX/bin/$util"
-done <"${RECIPE_DIR}/unix_bins.txt"
-
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
-    coreutils_bin="$PREFIX/bin/coreutils"
-
-    mkdir -p "$PREFIX/share/zsh/site-functions/"
-    mkdir -p "$PREFIX/share/bash-completion/completions/"
-    mkdir -p "$PREFIX/share/fish/vendor_completions.d/"
-    mkdir -p "$PREFIX/share/man/man1"
-
-    while read -r util; do
-        "$coreutils_bin" completion "$util" zsh > "$PREFIX/share/zsh/site-functions/_$util"
-        "$coreutils_bin" completion "$util" bash > "$PREFIX/share/bash-completion/completions/$util"
-        "$coreutils_bin" completion "$util" fish > "$PREFIX/share/fish/vendor_completions.d/$util.fish"
-        "$coreutils_bin" manpage "$util" > "$PREFIX/share/man/man1/$util.1"
-    done <"${RECIPE_DIR}/unix_bins.txt"
-fi
