df0ff805c792c6748ef9700af22e9d48f13e8a66
Brandon Maier <brandon.maier@gmail.com>
2025-02-17 07:42:36 -0600

    support cross-compile on unix

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,18 +9,22 @@ cargo install --bins --no-track --locked --root "${PREFIX}" --path . --features 
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-coreutils_bin="$PREFIX/bin/coreutils"
-
-mkdir -p "$PREFIX/share/zsh/site-functions/"
-mkdir -p "$PREFIX/share/bash-completion/completions/"
-mkdir -p "$PREFIX/share/fish/vendor_completions.d/"
-mkdir -p "$PREFIX/share/man/man1"
-
-"$coreutils_bin" --list >utils
-while IFS= read -r util; do
+while read -r util; do
     ln -sf coreutils "$PREFIX/bin/$util"
-    "$coreutils_bin" completion "$util" zsh > "$PREFIX/share/zsh/site-functions/_$util"
-    "$coreutils_bin" completion "$util" bash > "$PREFIX/share/bash-completion/completions/$util"
-    "$coreutils_bin" completion "$util" fish > "$PREFIX/share/fish/vendor_completions.d/$util.fish"
-    "$coreutils_bin" manpage "$util" > "$PREFIX/share/man/man1/$util.1"
-done <utils
+done <"${RECIPE_DIR}/unix_bins.txt"
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
+    coreutils_bin="$PREFIX/bin/coreutils"
+
+    mkdir -p "$PREFIX/share/zsh/site-functions/"
+    mkdir -p "$PREFIX/share/bash-completion/completions/"
+    mkdir -p "$PREFIX/share/fish/vendor_completions.d/"
+    mkdir -p "$PREFIX/share/man/man1"
+
+    while read -r util; do
+        "$coreutils_bin" completion "$util" zsh > "$PREFIX/share/zsh/site-functions/_$util"
+        "$coreutils_bin" completion "$util" bash > "$PREFIX/share/bash-completion/completions/$util"
+        "$coreutils_bin" completion "$util" fish > "$PREFIX/share/fish/vendor_completions.d/$util.fish"
+        "$coreutils_bin" manpage "$util" > "$PREFIX/share/man/man1/$util.1"
+    done <"${RECIPE_DIR}/unix_bins.txt"
+fi
