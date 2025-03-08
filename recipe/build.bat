ef2cc1c9d01ca59c6be36a7377dea47515a44233
Brandon Maier <brandon.maier@gmail.com>
2025-03-08 12:50:47 -0600

    test windows build with show-utils

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,5 +1,7 @@
-make PROFILE=release PREFIX="%CYGWIN_PREFIX%/Library" MULTICALL=n PROG_SUFFIX=.exe install
+.\util\show-utils.BAT >utils.txt
 if errorlevel 1 exit 1
+
+for /F "tokens=*" %%i in (utils.txt) do cargo install --bins --no-track --locked --root "%LIBRARY_PREFIX%" --path src/uu/%%i
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 if errorlevel 1 exit 1
