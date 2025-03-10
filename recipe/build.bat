91b2a189b64bca438104a6c1fdd05ef62039d2f0
Brandon Maier <brandon.maier@gmail.com>
2025-03-10 10:22:44 -0500

    Revert "test windows build with show-utils"
    This reverts commit ef2cc1c9d01ca59c6be36a7377dea47515a44233.

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,7 +1,5 @@
-.\util\show-utils.BAT >utils.txt
+make PROFILE=release PREFIX="%CYGWIN_PREFIX%/Library" MULTICALL=n PROG_SUFFIX=.exe install
 if errorlevel 1 exit 1
-
-for /F "tokens=*" %%i in (utils.txt) do cargo install --bins --no-track --locked --root "%LIBRARY_PREFIX%" --path src/uu/%%i
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 if errorlevel 1 exit 1
