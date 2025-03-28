7311205a16742909d25d7b225135695d9b848543
Brandon Maier <brandon.maier@gmail.com>
2025-03-28 16:15:13 -0500

    install windows binaries manually

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,7 +1,4 @@
-cargo build  --features "base32 base64 basename basenc cat cksum comm cp csplit cut date dd df dir dircolors dirname echo env expand expr factor false fmt fold hashsum head join link ln ls mkdir mktemp more mv nl nproc numfmt od paste pr printenv printf ptx pwd readlink realpath rm rmdir seq shred shuf sleep sort split sum sync tac tail tee test tr true truncate tsort unexpand uniq vdir wc whoami yes" --release --no-default-features
-if errorlevel 1 exit 1
-
-make PROFILE=release PREFIX="%CYGWIN_PREFIX%/Library" MULTICALL=n PROG_SUFFIX=.exe install
+for %%i in (base32 base64 basename basenc cat cksum comm cp csplit cut date dd df dir dircolors dirname echo env expand expr factor false fmt fold hashsum head join link ln ls mkdir mktemp more mv nl nproc numfmt od paste pr printenv printf ptx pwd readlink realpath rm rmdir seq shred shuf sleep sort split sum sync tac tail tee test tr true truncate tsort unexpand uniq vdir wc whoami yes) do cargo install --bins --no-track --locked --root "%LIBRARY_PREFIX%" --path src/uu/%%i
 if errorlevel 1 exit 1
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
