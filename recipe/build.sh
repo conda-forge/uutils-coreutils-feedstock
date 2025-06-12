ffd96cce6c58fedbb32785f07808b368d3efb8c4
Kerry Su <me@sshockwave.net>
2025-06-12 23:45:02 +0800

    Revert setting CARGO_TARGET_DIR
    Changed in 537f91a2780bb59afa3f0aed7d4ec4de56c6772f

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,6 @@ fi
 
 cargo build --release --features "${FEATURE_SET}"
 
-make PROFILE=Release PREFIX="${PREFIX}" PROG_SUFFIX= MULTICALL=y CARGO_TARGET_DIR="$(pwd)/target/release" install
+make PROFILE=Release PREFIX="${PREFIX}" PROG_SUFFIX= MULTICALL=y install
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
