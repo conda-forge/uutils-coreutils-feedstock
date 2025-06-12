5a61c0d00df55df01b698eed0ed6e4b9986ef7ac
Kerry Su <me@sshockwave.net>
2025-06-13 00:33:40 +0800

    Set LIBCLANG_PATH for bindgen

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,8 @@ set -ex
 FEATURE_SET="unix"
 if [[ "${target_platform}" == "osx"* ]]; then
     FEATURE_SET="macos"
+else
+    export LIBCLANG_PATH="${BUILD_PREFIX}/lib"
 fi
 
 cargo build --release --features "${FEATURE_SET}"
