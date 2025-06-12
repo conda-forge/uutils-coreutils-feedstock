537f91a2780bb59afa3f0aed7d4ec4de56c6772f
Kerry Su <me@sshockwave.net>
2025-06-12 22:46:03 +0800

    Update patches

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,6 @@ fi
 
 cargo build --release --features "${FEATURE_SET}"
 
-make PROFILE=Release PREFIX="${PREFIX}" PROG_SUFFIX= MULTICALL=y install
+make PROFILE=Release PREFIX="${PREFIX}" PROG_SUFFIX= MULTICALL=y CARGO_TARGET_DIR="$(pwd)/target/release" install
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
