8553bfbd10a69b3e659de3bbaaf3472a37585559
XhstormR <10527522+XhstormR@users.noreply.github.com>
2025-11-13 01:04:51 +0300

    add value check

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,10 +3,16 @@
 :: dump licenses
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
+for /f "usebackq delims=" %%A in (`where ln`) do set "LN=%%A" :: https://github.com/uutils/coreutils/issues/9244
+
+:: convert Windows format path to Unix format
+if "%LN%" == "" ( echo LN not set & goto :error)
+if "%PREFIX%" == "" ( echo PREFIX not set & goto :error)
+for /f "usebackq delims=" %%A in (`cygpath -u "%%LN%%"`) do set "LN=%%A"
+for /f "usebackq delims=" %%A in (`cygpath -u "%%PREFIX%%"`) do set "PREFIX=%%A"
+
 :: build
 set CARGO_BUILD_TARGET=
-for /f "usebackq delims=" %%A in (`which ln`) do set "LN=%%A" :: https://github.com/uutils/coreutils/issues/9244
-for /f "usebackq delims=" %%A in (`cygpath -u "%%PREFIX%%"`) do set "PREFIX=%%A" :: convert Windows format path to Unix format
 make PROFILE=release MULTICALL=y PREFIX="%PREFIX%" LN="%LN%" install || goto :error
 
 goto :EOF
