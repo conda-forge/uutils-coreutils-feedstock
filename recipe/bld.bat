7d0efe84126c0ec33a977a9c098baddfae8007c7
XhstormR <10527522+XhstormR@users.noreply.github.com>
2025-11-12 22:29:37 +0300

    fix ln

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,8 +5,9 @@ cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 :: build
 set CARGO_BUILD_TARGET=
+for /f "usebackq delims=" %%A in (`which ln`) do set "LN=%%A" :: https://github.com/uutils/coreutils/issues/9244
 for /f "usebackq delims=" %%A in (`cygpath -u "%%PREFIX%%"`) do set "PREFIX=%%A" :: convert Windows format path to Unix format
-make PROFILE=release MULTICALL=y PREFIX="%PREFIX%" install || goto :error
+make PROFILE=release MULTICALL=y PREFIX="%PREFIX%" LN="%LN%" install || goto :error
 
 goto :EOF
 
