37df714092b9acacc16821a598abf0bb9b217b85
XhstormR <10527522+XhstormR@users.noreply.github.com>
2025-11-12 16:36:40 +0300

    convert Windows format path to Unix format

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,7 @@ cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 :: build
 set CARGO_BUILD_TARGET=
+for /f "usebackq delims=" %%A in (`cygpath -u "%%PREFIX%%"`) do set "PREFIX=%%A" :: convert Windows format path to Unix format
 make PROFILE=release MULTICALL=y PREFIX="%PREFIX%" install || goto :error
 
 goto :EOF
