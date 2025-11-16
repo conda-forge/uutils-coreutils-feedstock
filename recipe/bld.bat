42f079170def5e0884f382b8abd1d0bad13df267
XhstormR <10527522+XhstormR@users.noreply.github.com>
2025-11-16 12:16:55 +0300

    make the link.exe use absolute paths

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,12 +4,15 @@
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 for /f "usebackq delims=" %%A in (`where ln`) do set "LN=%%A" :: https://github.com/uutils/coreutils/issues/9244
+for /f "usebackq delims=" %%A in (`where link`) do set "RUSTC_LINKER=%%A"
 
 :: convert Windows format path to Unix format
 if "%LN%" == "" ( echo LN not set & goto :error)
 if "%PREFIX%" == "" ( echo PREFIX not set & goto :error)
+if "%RUSTC_LINKER%" == "" ( echo RUSTC_LINKER not set & goto :error)
 for /f "usebackq delims=" %%A in (`cygpath -u "%%LN%%"`) do set "LN=%%A"
 for /f "usebackq delims=" %%A in (`cygpath -u "%%PREFIX%%"`) do set "PREFIX=%%A"
+for /f "usebackq delims=" %%A in (`cygpath -u "%%RUSTC_LINKER%%"`) do set "RUSTC_LINKER=%%A"
 
 :: build
 set CARGO_BUILD_TARGET=
