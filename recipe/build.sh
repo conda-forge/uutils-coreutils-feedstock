057e3bc72ef392c7f6072128a2968b34948fadf1
XhstormR <10527522+XhstormR@users.noreply.github.com>
2025-11-17 20:24:09 +0300

    convert_feedstock_to_v1_recipe_format (#25)
    * convert_feedstock_to_v1_recipe_format
    
    * update build number
    
    * update conda-forge.yml
    
    * MNT: Re-rendered with conda-smithy 3.53.1 and conda-forge-pinning 2025.11.17.12.05.4
    
    * update
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,9 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
+if [[ "$(uname)" == "Linux" ]]; then
+    export LIBCLANG_PATH=$BUILD_PREFIX/lib
+fi
+
 export -n CARGO_BUILD_TARGET # https://github.com/uutils/coreutils/issues/9206
 make PROFILE=release MULTICALL=y PREFIX="${PREFIX}" install
